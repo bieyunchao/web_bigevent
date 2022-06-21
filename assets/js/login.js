bfa0bd00ee8c098b80dcf892e439c6d652d4dcaa
bieyunchao <2813169990@qq.com>
2022-06-21 10:05:37 +0800

    修改注册表单的路径问题

--- assets/js/login.js
+++ assets/js/login.js
@@ -39,7 +39,7 @@ $(function () {
     e.preventDefault()
     // 2. 发起 Ajax 请求
     var data = { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }
-    $.post('  /api/reguser', data, function (res) {
+    $.post('/api/reguser', data, function (res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
